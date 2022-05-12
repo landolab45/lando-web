af216c9c3a0d39230c762341441bd8b9d176a02a
landolab45 <lando.lab45@gmail.com>
2022-05-12 09:17:55 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SISN - Software Information System Nusantara</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/animscroll.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <header id="header" class="fixed-top bg-white p-3 shadow-sm">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                        <img src="img/sisn-logo.png" alt="PT Software Information System Nusantara">
+                    </a>
+                
+                    <ul class="nav nav-pills">
+                        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Solution</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Product</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+        <main id="main">
+            <div id="jumbotron" class="container-fluid position-relative p-0">
+                <div class="parallax-window position-relative" data-parallax="scroll" data-image-src="asset/pexels-scott-webb-1029606.png">
+                    <div class="row">
+                        <div id="jumbotronText" class="position-absolute col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" style="height: 100%;">
+                            <div class="position-absolute top-50 start-50 translate-middle p-5 text-white" style="width: 75%;">
+                                <h1 class="fw-bold mb-4 lh-base" style="text-shadow: 2px 2px 2px rgba(0,0,0,0.8);">Leading BMC<br>Software Partner in <br>Indonesia</h1>
+                                <button type="button" class="btn btn-primary fs-5 px-4">See more</button>
+                            </div>
+                            <div class="position-absolute bottom-0 end-0 translate-middle-x">
+                                <div class="p-4 mb-4 rounded-0 border-0">
+                                    <img src="img/BMC_Elite-Partner.png" width="180">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--
+            <div id="solutions" class="position-relative p-5" style="background: #FCFCFC !important;">
+                <div class="container">
+                    <h2 class="display-6 fw-normal text-center mb-4">SOLUTION</h2>
+
+                    <div class="card-group">
+                        <div class="card mx-3 px-3 js-scroll fade-in border-0 bg-transparent">
+                            <div class="card-body">
+                                <h5 class="card-title fs-3 text-center">Card title</h5>
+                                <hr>
+                                <p class="card-text fs-5 fw-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                        <div class="card mx-3 px-3 js-scroll fade-in border-0 bg-transparent">
+                            <div class="card-body">
+                                <h5 class="card-title fs-3 text-center">Card title</h5>
+                                <hr>
+                                <p class="card-text fs-5 fw-light">This card has supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                        <div class="card mx-3 px-3 js-scroll fade-in border-0 bg-transparent">
+                            <div class="card-body">
+                                <h5 class="card-title fs-3 text-center">Card title</h5>
+                                <hr>
+                                <p class="card-text fs-5 fw-light">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            -->
+
+
+            <div id="about" class="position-relative p-5 bg-white">
+                <div class="container">
+                    <div class="row featurette">
+                        <div class="me-5 mb-5 text-center">
+                            <h2 class="featurette-heading fw-bold">Our Goal to <span class="text-primary">help you maximize your IT</span> Investments</h2>
+                        </div>
+                        <section class="scroll-container">
+                            <div class="scroll-caption js-scroll slide-left me-5">
+                                <p class="h4 fw-lighter mb-5 lh-lg">
+                                    From core to cloud to edge, BMC delivers the software and services that enable over 10.000 global customers, including 84% of the Forbers Global 100, to thrive in their ongoing evolution to an Autonomous Digital Enterprise.
+                                </p>
+                                <p class="h4 lh-lg" style="font-size: 1.5rem !important;">
+                                    Run and Reinvent Your Business with BMC
+                                </p>
+                            </div>
+                            <div class="scroll-element js-scroll slide-right text-end">
+                                <img src="asset/1ecbca16-5bc4-4be9-81cd-d62d775fbbef 1.png" width="600" />
+                            </div>                            
+                        </section>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div id="services" class="position-relative p-5">
+                <div class="container">
+                    <div class="text-center mb-5 mx-auto">
+                        <h2 class="display-6 fw-normal text-primary sisn-title">
+                            <span>Services</span>
+                        </h2>
+                    </div>
+                    <div class="row justify-content-evenly mb-4">
+                        <div class="d-flex col-4 row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col-auto text-center d-none d-lg-block" style="background-color: rgb(13, 110, 253, 0.2);">
+                                <img class="p-4" src="asset/implementation 1.png">
+                            </div>
+                            <div class="col d-flex my-auto flex-column position-static text-center">
+                                <h3 class="p-4" class="mb-0 ">Implementation</h3>
+                            </div>
+                        </div>
+                        <div class="d-flex col-4 row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col-auto text-center d-none d-lg-block" style="background-color: rgba(220, 53, 69, 0.2);">
+                                <img class="p-4" src="asset/presentation 2.png">
+                            </div>
+                            <div class="col d-flex my-auto flex-column position-static text-center">
+                                <h3 class="p-4" class="mb-0 ">Training</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-evenly mb-4">
+                        <div class="d-flex col-4 row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col-auto text-center d-none d-lg-block" style="background-color: rgba(255, 193, 7, 0.2);">
+                                <img class="p-4" src="asset/consult 1.png">
+                            </div>
+                            <div class="col d-flex my-auto flex-column position-static text-center">
+                                <h3 class="p-4" class="mb-0 ">Consulting</h3>
+                            </div>
+                        </div>
+                        <div class="d-flex col-4 row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col-auto text-center d-none d-lg-block" style="background-color: rgba(25, 135, 84, 0.2);">
+                                <img class="p-4" src="asset/setting 1.png">
+                            </div>
+                            <div class="col d-flex my-auto flex-column position-static text-center">
+                                <h3 class="p-4" class="mb-0 ">Support</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- <div class="container">
+                <hr class="featurette-divider">
+            </div> -->
+
+            <div id="clients" class="position-relative p-5" style="background-color: #ebedf8 !important;">
+                <div class="container">
+                    <div class="text-center px-4 mb-5 mx-auto" style="width: 75%;">
+                        <h2 class="display-6 fw-normal mb-4">Our Customer</h2>
+                        <p class="fs-5 text-muted">We are proud to work with selected high-quality partners that bring an added dimension to our products and services to better serve our customers.</p>    
+                    </div>
+                    <div class="js-scroll fade-in-bottom text-center">
+                        <img src="asset/client/logo-bca-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Bank Central Asia Tbk" alt="PT Bank Central Asia Tbk">
+                        <img src="asset/client/logo-cimb-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Bank CIMB Niaga Tbk" alt="PT Bank CIMB Niaga Tbk">
+                        <img src="asset/client/logo-bmri-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Bank Mandiri (Persero) Tbk" alt="PT Bank Mandiri (Persero) Tbk">
+                        <img src="asset/client/logo-bni-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Bank Nasional Indonesia (Persero) Tbk" alt="PT Bank Nasional Indonesia (Persero) Tbk">
+                        <img src="asset/client/logo-bnilife-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT BNI Life Insurance" alt="PT BNI Life Insurance">
+                        <img src="asset/client/logo-artajasa-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Artajasa Pembayaran Elektronis" alt="PT Artajasa Pembayaran Elektronis">
+                        <img src="asset/client/logo-primacom-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Primacom Interbuana" alt="PT Primacom Interbuana">
+                        <img src="asset/client/logo-kai-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Kereta Api Indonesia (Persero)" alt="PT Kereta Api Indonesia (Persero)">
+                        <img src="asset/client/logo-ggrm-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Gudang Garam Tbk" alt="PT Gudang Garam Tbk">
+                        <img src="asset/client/logo-sm-color.png" height="60" class="img-thumbnail mx-3 mb-4" data-bs-toggle="tooltip" data-bs-placement="top" title="PT Surya Madistrindo" alt="PT Surya Madistrindo">
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        
+        <footer class="text-light">
+            <div class="container">
+                <footer class="py-5">
+                  <div class="row">
+                    <div class="col-2">
+                      <h5 class="fw-bold mb-4">About Us</h5>
+                      <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Company Profile</a></li>
+                      </ul>
+                    </div>
+              
+                    <div class="col-2">
+                      <h5 class="fw-bold mb-4">Services</h5>
+                      <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Implementation</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Consulting</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Training</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Support</a></li>
+                      </ul>
+                    </div>
+              
+                    <div class="col-2">
+                      <h5 class="fw-bold mb-4">Product</h5>
+                      <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">IT Service Management</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Automation</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Operations</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Mainframe</a></li>
+                      </ul>
+                    </div>
+                    <div class="col-2">
+                    </div>
+                    <div class="col-4">
+                        <h5 class="fw-bold mb-4">Contact</h5>
+                        <ul class="nav flex-column">
+                          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Home</a></li>
+                          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Features</a></li>
+                          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Pricing</a></li>
+                          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">FAQs</a></li>
+                          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">About</a></li>
+                        </ul>
+                    </div>
+
+                  </div>
+              
+                  <div class="d-flex justify-content-between pt-4 mt-4 border-top border-secondary">
+                    <p class="fw-lighter">&copy; Copyright 2022 PT Software Information System Nusantara</p>
+                  </div>
+                </footer>
+            </div>            
+        </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> 
+        <script src="js/parallax.min.js"></script>
+        <script src="js/animscroll.js"></script>
+        <script>
+            $(document).on('scroll', function(){
+                // $('#jumbotronText').css("left", Math.max(0 - 0.15*window.scrollY) + "px");
+                $('#jumbotronText').css("bottom", Math.max(0 - 0.5*window.scrollY) + "px");
+                // $('#jumbotronText').css("opacity", Math.max(1 - 0.0005*window.scrollY));
+                $('#jumbotronText').css("opacity", Math.max(2.25 - (7.5*window.scrollY/window.innerHeight)));
+            })
+        </script>
+        <script>
+            $(document).ready(function(){
+                $(this).scrollTop(1);
+            });
+
+            let w = window.innerWidth;
+            let h = window.innerHeight;
+            let headerHeight = document.getElementById('header').offsetHeight;
+            
+            function jumbotronBtn() {
+                window.scrollTo(0, (h - headerHeight)); 
+            }
+
+            /* MAIN MARGIN TOP */
+            document.getElementById("main").style.marginTop = headerHeight + "px";
+
+            /* JUMBOTRON */
+            // document.getElementById("jumbotron").style.height = (window.innerHeight - headerHeight) + "px";
+
+        </script>
+
+    </body>
+</html> 
